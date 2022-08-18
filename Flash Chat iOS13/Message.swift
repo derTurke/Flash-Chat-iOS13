1f8f06d8739de488f1632750ca66f44a64496c32
GÜRHAN YUVARLAK <76452852+derTurke@users.noreply.github.com>
2022-08-19 00:56:09 +0300

    Initial commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Messages.swift
+//  Flash Chat iOS13
+//
+//  Created by GÜRHAN YUVARLAK on 9.08.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+struct Message {
+    let sender: String
+    let body: String
+}
